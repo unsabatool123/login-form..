9b30cd3113155551395da6c2e28d3de23595f9ab
Syeda Unsa <168438643+unsabatool123@users.noreply.github.com>
2024-05-27 11:57:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function takevalue(){
+var name =document.getElementById
+("name").value;
+
+var email =document.getElementById
+("email").value;
+
+
+var password =document.getElementById
+("password").value;
+
+
+document.write("your name is = " + name +" <br>"); 
+
+document.write("your email is = " + email +" <br>" );
+
+document.write("your password is = " + password +" <br>" );
+
+
+}
+
+
+
+
+
